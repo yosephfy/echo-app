8eaea534909f9c138f23315360dd9c22deefc650
Yoseph Tezera <70306341+yosephfy@users.noreply.github.com>
2025-08-28 22:01:30 -0400

    feat: configure backend server to use environment variables for port and host

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(
+    process.env.BACKEND_PORT || 3000,
+    process.env.BACKEND_HOST || 'localhost',
+  );
 }
 bootstrap();
