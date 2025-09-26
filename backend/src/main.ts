53a7d53f7d303ab4e23cdbfe4ec1cf569e33b59b
Yoseph Tezera <70306341+yosephfy@users.noreply.github.com>
2025-09-25 23:26:17 -0400

    misc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT || process.env.BACKEND_PORT || 3000;
   const host = process.env.BACKEND_HOST || '0.0.0.0';
+
   await app.listen(port, host);
 }
 bootstrap();
