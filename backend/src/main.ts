1df8314c814c1110cd877d2b1663f2acb298ac67
Yoseph Tezera <70306341+yosephfy@users.noreply.github.com>
2025-09-25 20:55:39 -0400

    added env.PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(
-    process.env.BACKEND_PORT || 3000,
-    process.env.BACKEND_HOST || 'localhost',
-  );
+  const port = process.env.PORT || process.env.BACKEND_PORT || 3000;
+  const host = process.env.BACKEND_HOST || '0.0.0.0';
+  await app.listen(port, host);
 }
 bootstrap();
